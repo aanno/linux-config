eaa66a3a85c8ff5812cfc1cda0f78364ac8cee9f
Thomas Pasch <aannoaanno@gmail.com>
2017-10-07 18:31:37 +0200

    fixed dnc-compose.sh

--- docker-compose/nextcloud/dnc-compose.sh
+++ docker-compose/nextcloud/dnc-compose.sh
@@ -8,6 +8,9 @@ export VERSION="11.0.5"
 export DOMAIN="breitbandig.de,www.breitbandig.de,mail.breitbandig.de,netzgeneration.com,www.netzgeneration.com,mail.netzgeneration.com,v22017092131153710.goodsrv.de"
 export EMAIL="aannoaanno@gmail.com"
 
-pushd /root/bin
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $DIR
   docker-compose up
 popd
