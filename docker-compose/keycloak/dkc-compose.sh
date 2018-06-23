4359731b579636976485c386741900fadb6b2d74
Thomas Pasch <aannoaanno@gmail.com>
2018-06-23 12:44:49 +0200

    keycloak on mariadb

--- docker-compose/keycloak/dkc-compose.sh
+++ docker-compose/keycloak/dkc-compose.sh
@@ -3,8 +3,14 @@
 
 
 # export BASE="/home/dockeruser/nextcloud"
-export BASE="."
+export BASE="/home/dockeruser/keycloak"
+# not used 
 export BASE_PATH="/keycloak"
+
+if [ -z $KEYCLOAK_DB_PASSWORD ]; then
+  echo "KEYCLOAK_DB_PASSWORD not set"
+  exit -1
+fi
 
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
