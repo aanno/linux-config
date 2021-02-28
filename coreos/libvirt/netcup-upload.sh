1780b11c7d81be1101a446fc87ecce19da564441
aanno <aanno@users.noreply.github.com>
2021-02-28 09:39:29 +0100

    fixed upload

--- coreos/libvirt/netcup-upload.sh
+++ coreos/libvirt/netcup-upload.sh
@@ -2,8 +2,8 @@
 
 CONFIG_FILE_NAME=my
 
-# absolute path needed
-IMAGE="$HOME/.local/share/libvirt/images/fedora-coreos-33.20210201.3.0-qemu.x86_64.qcow2"
+# IMAGE="$HOME/.local/share/libvirt/images/fedora-coreos-33.20210201.3.0-qemu.x86_64.qcow2"
+IMAGE=fcos.qcow2
 
 scp -P 2222 "$IMAGE" "$NC_USER@46.38.225.190:/image"
 
