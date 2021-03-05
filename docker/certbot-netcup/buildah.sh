2f6d92185c19603fa1dc3d06d5dcc483dae1abf9
aanno <aanno@users.noreply.github.com>
2021-03-05 10:28:50 +0100

    fixes

--- docker/certbot-netcup/buildah.sh
+++ docker/certbot-netcup/buildah.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x 
 
 export VERSION="v1.13.0"
+export IMAGE="docker.io/aanno/certbot-netcup:$VERSION"
 
 envsubst <Dockerfile.envsub >Dockerfile
-buildah bud -t docker.io/aanno/certbot-netcup:$VERSION .
+buildah bud -t "$IMAGE" .
+podman push "$IMAGE"
