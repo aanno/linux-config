7fc49814d37f91d383e18ed3bd60af81aac35896
aanno <aanno@users.noreply.github.com>
2021-03-28 18:50:17 +0200

    use dnscrypt-proxy

--- coreos/libvirt/create-local-vm.sh
+++ coreos/libvirt/create-local-vm.sh
@@ -16,5 +16,6 @@ virt-install --connect="qemu:///system" --name="${VM_NAME}" --vcpus="${VCPUS}" -
         --os-variant="fedora-coreos-$STREAM" --import --graphics=none \
         --disk="size=${DISK_GB},backing_store=${IMAGE}" \
         --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}" \
+        --mac="52:54:00:a8:c2:c8" \
         --network "${NETWORK}"
 
