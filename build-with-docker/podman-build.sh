f271c82425e7070af97fb0ea5f83339d590b6f63
aanno <aanno@users.noreply.github.com>
2021-05-08 10:45:04 +0200

    improved spotifyd build (ongoing)

--- build-with-docker/podman-build.sh
+++ build-with-docker/podman-build.sh
@@ -5,10 +5,19 @@ set -xe
 # https://stackoverflow.com/questions/30646943/how-to-avahi-browse-from-a-docker-container
 CWD=`pwd`
 
-mkdir -p $CWD/opt/owntone
-rm -rf $CWD/opt/owntone/* || true
-podman build --pull --rm -t owntone -v $CWD/opt/owntone:/opt/owntone:z \
-  -f Containerfile.ubuntu-hirsute
+mkdir -p $CWD/opt/owntone \
+  $CWD/var/lib/apt $CWD/var/cache/apt \
+  $CWD/var/lib/dnf $CWD/var/cache/dnf \
+  $CWD/.cargo;
+rm -rf $CWD/opt/owntone/* || true;
+podman build --pull --rm -t owntone \
+  -v $CWD/var/lib/apt:/var/lib/apt:z \
+  -v $CWD/var/cache/apt:/var/cache/apt:z \
+  -v $CWD/var/lib/dnf:/var/lib/dnf:z \
+  -v $CWD/var/cache/dnf:/var/cache/dnf:z \
+  -v $CWD/opt/owntone:/opt/owntone:z \
+  -v $CWD/.cargo:/root/.cargo:z \
+  -f Containerfile.spotifyd.ubuntu-hirsute;
 tar cvfz owntone.tar.gz ./opt/
 rm -rf ./opt
 podman rmi localhost/owntone
