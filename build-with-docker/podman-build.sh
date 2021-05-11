253bb58376c4c13b830cd5edcd983a5c6c90ecd2
aanno <aanno@users.noreply.github.com>
2021-05-11 14:52:23 +0200

    fixes

--- build-with-docker/podman-build.sh
+++ build-with-docker/podman-build.sh
@@ -5,6 +5,9 @@ set -xe
 # https://stackoverflow.com/questions/30646943/how-to-avahi-browse-from-a-docker-container
 CWD=`pwd`
 
+rm -rf ./opt || true
+podman rmi localhost/owntone || true
+
 mkdir -p $CWD/opt/owntone \
   $CWD/var/lib/apt $CWD/var/cache/apt \
   $CWD/var/lib/dnf $CWD/var/cache/dnf \
@@ -19,5 +22,6 @@ podman build --pull --rm -t owntone \
   -v $CWD/.cargo:/root/.cargo:z \
   -f Containerfile.nymphcast.fedora34;
 tar cvfz owntone.tar.gz ./opt/
-rm -rf ./opt
-podman rmi localhost/owntone
+
+# rm -rf ./opt
+# podman rmi localhost/owntone
