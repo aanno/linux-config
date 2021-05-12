21da458c24f3445afdc7a3846e9c2a88a1e16f8d
aanno <aanno@users.noreply.github.com>
2021-05-12 16:00:07 +0200

    more tries on nothing provides libnymphcast.so.0.1 problem

--- build-with-docker/pacur-pack.sh
+++ build-with-docker/pacur-pack.sh
@@ -5,5 +5,6 @@ set -xe
 # https://stackoverflow.com/questions/30646943/how-to-avahi-browse-from-a-docker-container
 CWD=`pwd`
 
+rm nymphcast-all* || true
 podman run --rm -t -v $CWD:/pacur:z localhost/pacur/fedora-34
 rpm -qlp nymphcast-all-0.0.1-1.fc34.x86_64.rpm
