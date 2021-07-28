12478653ec750f8cbf8c4cbb1aaffb4b3b1e9622
aanno <aanno@users.noreply.github.com>
2021-07-28 16:11:19 +0200

    towards envsubst config

--- docker-compose/nextcloud-spreed/scripts/spreed-start.sh
+++ docker-compose/nextcloud-spreed/scripts/spreed-start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -x
 
+set -xe
+
+GIT_ROOT=`git rev-parse --show-toplevel`
+
+pushd "$GIT_ROOT/docker-compose/nextcloud-spreed"
+
+source "scripts/env.sh"
+
+rm docker-compose.yml server.conf || true
+envsubst <docker-compose.in.yml >docker-compose.yml
+envsubst <server.in.conf >server.conf
 podman-compose -p spreed up docker-compose.yml 
 
+popd
