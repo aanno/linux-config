90a0f0c4c255ffe71d8906bb61aa693bc7e4b8cc
aanno <aanno@users.noreply.github.com>
2022-11-05 13:46:54 +0100

    build-with-docker: update to kinetic

--- build-with-docker/podman-build.sh
+++ build-with-docker/podman-build.sh
@@ -22,7 +22,7 @@ podman build --pull -t owntone \
   -v $CWD/opt/owntone:/opt/owntone:z \
   -v $CWD/.cargo:/root/.cargo:z \
   -v $CWD/.rustup:/root/.rustup:z \
-  -f Containerfile.spotifyd.ubuntu-jammy;
+  -f Containerfile.spotifyd.ubuntu-kinetic;
 tar cvfz owntone.tar.gz ./opt/
 
 # rm -rf ./opt
