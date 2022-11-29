01656a6932f06eeecbdb56a336db61606cfce038
aanno <aanno@users.noreply.github.com>
2022-11-29 20:21:09 +0100

    wp fixes

--- docker-compose/wordpress/db/create-user-db.sh
+++ docker-compose/wordpress/db/create-user-db.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 
+source "../db.env"
 cat db.env | sed -e 's/^/export /' >db-export.env
-source db-export.env
-envsubst <db/create.in.sql >db/create.sql 
+source "db-export.env"
+echo "WORDPRESS_DB_HOST is $WORDPRESS_DB_HOST"
+
+envsubst <create.in.sql >create.sql
+cat create.sql
 
 # https://stackoverflow.com/questions/34779894/executing-sql-scripts-on-docker-container
-podman cp db/create.sql ${WORDPRESS_DB_HOST}:/
+podman cp create.sql ${WORDPRESS_DB_HOST}:/
 podman exec ${WORDPRESS_DB_HOST} sh -c 'mysql -u root --password=$MYSQL_ROOT_PASSWORD -B </create.sql'
 
 echo "mysql returns: $?"
