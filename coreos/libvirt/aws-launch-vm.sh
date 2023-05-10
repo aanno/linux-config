7871877f1a8934fb70d26f8b438922356124d074
aanno <aanno@users.noreply.github.com>
2023-05-10 22:09:40 +0200

    added spot options

--- coreos/libvirt/aws-launch-vm.sh
+++ coreos/libvirt/aws-launch-vm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # make a local vm from coreos image
 
 set -eu
@@ -8,12 +8,18 @@ CONFIG_FILE_NAME=my
 VM_NAME="fcos-37"
 TMP=`mktemp -d`
 
+# g2.2xlarge - 1 GPU Nvidia K520
+# p2.xlarge - 1 GPU Nvidia K80
+# 
+# t2.micro - kostenlos
+# t3.small - known to work
+
 NAME='fcos1'
 SSHKEY='redsnapper-tp'     # the name of your SSH key: `aws ec2 describe-key-pairs`
 IMAGE='ami-097f5ccd7b9deca93'     # the AMI ID found on the download page
 DISK='20'           # the size of the hard disk
 REGION='eu-central-1'  # the target region
-TYPE='t3.small'     # the instance type
+TYPE='t2.micro'     # the instance type
 SUBNET='subnet-ae95e7c4' # the subnet: `aws ec2 describe-subnets`
 SECURITY_GROUPS='sg-08c516b4328e8e738' # the security group `aws ec2 describe-security-groups`
 # USERDATA='/path/to/config.ign' # path to your Ignition config
@@ -39,8 +45,10 @@ aws ec2 run-instances                     \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" \
     --block-device-mappings "VirtualName=/dev/xvda,DeviceName=/dev/xvda,Ebs={VolumeSize=${DISK}}" \
     --user-data "file://${IGNITION_CONFIG}"      \
+    --instance-market-options file://spot-options.json \
     >run.log.json
 
+#--instance-market-options file://spot-options.json \
 #--user-data "file://${IGNITION_CONFIG}"      \
 #--user-data "https://breitbandig.de/nextcloud/s/TRAqLnHBeMqgZtR/download?path=.&files=my.ign" \
 
