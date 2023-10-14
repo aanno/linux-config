a53db0686c70a9991fee3997bfb0480a1aa7be04
aanno <aanno@users.noreply.github.com>
2023-10-14 13:09:21 +0200

    improved s5cmd-wasabi

--- scripts/s5cmd-wasabi.sh
+++ scripts/s5cmd-wasabi.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 # https://knowledgebase.wasabi.com/hc/en-us/articles/360044600552-How-do-I-use-s5cmd-with-Wasabi-
 # https://knowledgebase.wasabi.com/hc/en-us/articles/360015106031-What-are-the-service-URLs-for-Wasabi-s-different-storage-regions-
 # https://github.com/peak/s5cmd
+
+if [[ -z $WASABI_PROFILE ]]; then
+  WASABI_PROFILE=wasabi-ro
+fi
 
 export AWS_REGION=eu-central-2
 podman run --rm \
@@ -9,7 +13,7 @@ podman run --rm \
    -v ~/.aws:/root/.aws:z \
    -e "AWS_REGION=${AWS_REGION}" \
   docker.io/peakcom/s5cmd \
-  --profile=wasabi-ro \
+  --profile=${WASABI_PROFILE} \
   --endpoint-url=https://s3.eu-central-2.wasabisys.com \
   $*
 
