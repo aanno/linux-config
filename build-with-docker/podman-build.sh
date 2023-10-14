835b13e43d1042b65240f8122262103db4090db9
aanno <aanno@users.noreply.github.com>
2023-10-14 11:10:19 +0200

    tensorrt

--- build-with-docker/podman-build.sh
+++ build-with-docker/podman-build.sh
@@ -22,7 +22,7 @@ podman build --pull -t owntone \
   -v $CWD/opt/owntone:/opt/owntone:z \
   -v $CWD/.cargo:/root/.cargo:z \
   -v $CWD/.rustup:/root/.rustup:z \
-  -f Containerfile.obs-bgremoval.fedora38;
+  -f Containerfile.tensorrt.fedora38;
 tar cvfz owntone.tar.gz ./opt/
 
 # rm -rf ./opt
