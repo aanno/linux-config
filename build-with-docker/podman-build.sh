122594c4958dfa544954ad39d874aa8eca886c8e
aanno <aanno@users.noreply.github.com>
2023-10-29 17:19:38 +0100

    update to ubuntu-mantic/23.10

--- build-with-docker/podman-build.sh
+++ build-with-docker/podman-build.sh
@@ -22,7 +22,7 @@ podman build --pull -t owntone \
   -v $CWD/opt/owntone:/opt/owntone:z \
   -v $CWD/.cargo:/root/.cargo:z \
   -v $CWD/.rustup:/root/.rustup:z \
-  -f Containerfile.tensorrt.fedora38;
+  -f Containerfile.spotifyd.ubuntu-mantic;
 tar cvfz owntone.tar.gz ./opt/
 
 # rm -rf ./opt
