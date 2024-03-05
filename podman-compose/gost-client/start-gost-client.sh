78aefca04682d8c34d12e5c98a282006434dc909
aanno <aanno@users.noreply.github.com>
2024-03-05 15:00:29 +0100

    fixes in gost-client

--- podman-compose/gost-client/start-gost-client.sh
+++ podman-compose/gost-client/start-gost-client.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -x
 
 . .env
-export REMOTE_PROXY="${REMOTE_PROXY_HOST}:${REMOTE_PROXY_PORT}"
-export REMOTE_SOCKS="${REMOTE_SOCKS_HOST}:${REMOTE_SOCKS_PORT}"
+REMOTE_PROXY="${REMOTE_PROXY_HOST}:${REMOTE_PROXY_PORT}"
+REMOTE_SOCKS="${REMOTE_SOCKS_HOST}:${REMOTE_SOCKS_PORT}"
+export REMOTE_PROXY REMOTE_SOCKS REMOTE_PROXY_USER REMOTE_PROXY_PW
 envsubst <config/gost-subst.yml >config/gost.yml
 
 podman-compose down
-podman-compose  up -d
+podman-compose up -d
+
+sleep 2
+# test
+curl --proxy-user ${REMOTE_PROXY_USER}:${REMOTE_PROXY_PW} --proxy "http://localhost:8080" https://www.google.de
+curl --proxy-user ${REMOTE_PROXY_USER}:${REMOTE_PROXY_PW} --socks5 "http://localhost:1080" https://www.google.de
+
 podman-compose logs -f
