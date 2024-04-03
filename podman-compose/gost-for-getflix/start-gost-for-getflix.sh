0b5c4250b100b718965e20a9c89302d8263248b8
aanno <aanno@users.noreply.github.com>
2024-04-03 17:08:09 +0200

    added gost-for-getflix

---
+++ podman-compose/gost-for-getflix/start-gost-for-getflix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+. .env
+REMOTE_PROXY="${REMOTE_PROXY_HOST}:${REMOTE_PROXY_PORT}"
+REMOTE_SOCKS="${REMOTE_SOCKS_HOST}:${REMOTE_SOCKS_PORT}"
+export REMOTE_PROXY REMOTE_SOCKS REMOTE_PROXY_USER REMOTE_PROXY_PW
+envsubst <config/gost-subst.yml >config/gost.yml
+
+podman-compose down
+podman-compose up -d
+
+podman-compose logs -f
