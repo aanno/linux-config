37ca3abebb9cbc6ac8a82b1e97f252ba16e407d5
aanno <aanno@users.noreply.github.com>
2024-12-20 18:02:32 +0100

    some tries with ghost-for-netflix

--- podman-compose/gost-for-getflix/start-gost-for-getflix.sh
+++ podman-compose/gost-for-getflix/start-gost-for-getflix.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -x
 
+if [ -z $DOCKER ]; then
+  export DOCKER=podman
+fi
+
 . .env
 REMOTE_PROXY="${REMOTE_PROXY_HOST}:${REMOTE_PROXY_PORT}"
 REMOTE_SOCKS="${REMOTE_SOCKS_HOST}:${REMOTE_SOCKS_PORT}"
 export REMOTE_PROXY REMOTE_SOCKS REMOTE_PROXY_USER REMOTE_PROXY_PW
 envsubst <config/gost-subst.yml >config/gost.yml
 
-podman-compose down
-podman-compose up -d
-
-podman-compose logs -f
+$DOCKER compose down
+$DOCKER compose up -d
+$DOCKER compose logs -f
