c88c8af5fd27cefa25b0d2c4cf0ba7f237a1e9e1
aanno <aanno@users.noreply.github.com>
2024-12-21 18:06:50 +0100

    netcup: iso is unsuccessful on 1st try

--- coreos/server-f41/scripts/coreos-netcup-iso-install.sh
+++ coreos/server-f41/scripts/coreos-netcup-iso-install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
-
-# https://docs.fedoraproject.org/en-US/fedora-coreos/getting-started/
+# https://docs.fedoraproject.org/en-US/fedora-coreos/sysconfig-network-configuration/
+#
+# NOT WORKING! (at least on arm64)
+# tty0 error message:
+# BdsDxe: failed to load Boot004 "UEFI PXEv4 (MAC: )"
+#
+# Perhaps secure boot problem?
+# https://github.com/canonical/lxd/issues/7191
+# boot with
+# security.secureboot=false
 
 export GIT_ROOT=`git rev-parse --show-toplevel`
 pushd $GIT_ROOT/coreos/server-f41
