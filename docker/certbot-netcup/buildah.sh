45db42be2f5fafd5a0ac0f1133b4487c5fad0d65
aanno <aanno@users.noreply.github.com>
2025-02-23 15:39:15 +0000

    consider arch for certbot-netcup

--- docker/certbot-netcup/buildah.sh
+++ docker/certbot-netcup/buildah.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x 
 
-export VERSION="v2.10.0"
-export IMAGE="docker.io/aanno/certbot-netcup:$VERSION"
+export VERSION="v3.2.0"
+export ARCH=`uname -m`
+export IMAGE_VERSION="${VERSION}-${ARCH}"
+export IMAGE="docker.io/aanno/certbot-netcup:$IMAGE_VERSION"
 
 envsubst <Dockerfile.envsub >Dockerfile
 
