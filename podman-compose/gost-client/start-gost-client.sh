e9438dea38c63edd48c4cd0704c5d6c189e10b0f
aanno <aanno@users.noreply.github.com>
2025-03-01 21:10:32 +0000

    try on gost-client

--- podman-compose/gost-client/start-gost-client.sh
+++ podman-compose/gost-client/start-gost-client.sh
@@ -3,7 +3,7 @@
 . .env
 REMOTE_PROXY="${REMOTE_PROXY_HOST}:${REMOTE_PROXY_PORT}"
 REMOTE_SOCKS="${REMOTE_SOCKS_HOST}:${REMOTE_SOCKS_PORT}"
-export REMOTE_PROXY REMOTE_SOCKS REMOTE_PROXY_USER REMOTE_PROXY_PW
+export REMOTE_PROXY REMOTE_SOCKS REMOTE_PROXY_PORT REMOTE_SOCKS_PORT REMOTE_PROXY_USER REMOTE_PROXY_PW
 envsubst <config/gost-subst.yml >config/gost.yml
 
 podman-compose down
@@ -11,7 +11,7 @@ podman-compose up -d
 
 sleep 2
 # test
-curl --proxy-user ${REMOTE_PROXY_USER}:${REMOTE_PROXY_PW} --proxy "http://localhost:8080" https://www.google.de
-curl --proxy-user ${REMOTE_PROXY_USER}:${REMOTE_PROXY_PW} --socks5 "http://localhost:1080" https://www.google.de
+curl --proxy-user ${REMOTE_PROXY_USER}:${REMOTE_PROXY_PW} --proxy "http://${REMOTE_PROXY}" https://www.google.de
+curl --proxy-user ${REMOTE_PROXY_USER}:${REMOTE_PROXY_PW} --socks5 "http://${REMOTE_SOCKS}" https://www.google.de
 
 podman-compose logs -f
