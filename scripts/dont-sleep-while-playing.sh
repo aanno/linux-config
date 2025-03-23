09742ed26714087f47cee549db2964ba0f68b0ad
aanno <aanno@users.noreply.github.com>
2025-03-23 09:35:26 +0100

    dont-sleep: udev rule and perms

--- scripts/dont-sleep-while-playing.sh
+++ scripts/dont-sleep-while-playing.sh
@@ -3,6 +3,13 @@
 # cp -i /usr/lib/systemd/system/ydotool.service .config/systemd/user/
 # systemctl start --user ydotool.service
 
+# https://discussion.fedoraproject.org/t/implications-of-change-permissions-on-uinput/128865
+# sudo groupadd -r uinput
+# sudo usermod -a -G uinput my_username
+# sudo udevadm control --reload-rules
+# echo "SUBSYSTEM==\"misc\", KERNEL==\"uinput\", GROUP=\"uinput\", MODE=\"0660\"" | sudo tee /etc/udev/rules.d/uinput.rules
+# echo uinput | sudo tee /etc/modules-load.d/uinput.conf
+
 State=1
 # while [ State != 0 ]; do
 while true; do
