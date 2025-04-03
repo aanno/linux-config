043a9576d85ca404862f7e83c992599c42d0c3f6
aanno <aanno@users.noreply.github.com>
2025-04-03 10:38:52 +0200

    improved snapcast-prepare.sh

--- scripts/snapcast-prepare.sh
+++ scripts/snapcast-prepare.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -x
 
 killall snapserver pw-record || true
-sudo modprobe snd_aloop || true
+if lsmod | grep -q snd_aloop; then
+  echo "snd_aloop already loaded"
+else
+  sudo modprobe snd_aloop || true
+fi
+rm -f /tmp/snapfifo
 mkfifo /tmp/snapfifo || true
 snapserver &
 pw-record -P stream.capture.sink=true --target alsa_output.platform-snd_aloop.0.analog-stereo - >/tmp/snapfifo &
+xdg-open http://localhost:1780/ || true
