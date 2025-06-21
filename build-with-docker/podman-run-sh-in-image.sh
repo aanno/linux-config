2a423789cc74a91e48b51632aeed889adb994522
aanno <aanno@users.noreply.github.com>
2025-06-21 10:35:58 +0200

    fixes

--- build-with-docker/podman-run-sh-in-image.sh
+++ build-with-docker/podman-run-sh-in-image.sh
@@ -1,7 +1,26 @@
 #!/bin/bash -x
 
-# https://stackoverflow.com/questions/30646943/how-to-avahi-browse-from-a-docker-container
+set -xe
 
-podman run -v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket:z \
-  -v /var/run/dbus:/var/run/dbus:z \
-  --network=host --rm -it $1 bash
+# https://stackoverflow.com/questions/30646943/how-to-avahi-browse-from-a-docker-container
+CWD=`pwd`
+
+rm *.gz *.tgz || true
+podman rmi localhost/owntone || true
+
+mkdir -p $CWD/opt/owntone \
+  $CWD/var/lib/apt $CWD/var/cache/apt \
+  $CWD/var/lib/dnf $CWD/var/cache/dnf \
+  $CWD/.cargo $CWD/.rustup;
+# only nymphcast
+mkdir -p opt/owntone/include/nymph opt/owntone/lib
+rm -rf $CWD/opt/owntone/* || true;
+podman run --rm -it \
+  -v $CWD/var/lib/apt:/var/lib/apt:z \
+  -v $CWD/var/cache/apt:/var/cache/apt:z \
+  -v $CWD/var/lib/dnf:/var/lib/dnf:z \
+  -v $CWD/var/cache/dnf:/var/cache/dnf:z \
+  -v $CWD/opt:/opt:z \
+  -v $CWD/.cargo:/root/.cargo:z \
+  -v $CWD/.rustup:/root/.rustup:z \
+  snapcast bash
