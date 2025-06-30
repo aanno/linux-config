a6ec7b65d9ee650c4f517da88634d301527bb50f
aanno <aanno@users.noreply.github.com>
2025-06-30 08:07:38 +0200

    home/quabbe/bin

---
+++ home/quabbe/bin/fsck-enc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+# sudo powertop --auto-tune
+ls /dev/sd*
+sudo fsck /dev/sda1 -y
+sudo fsck /dev/sda2 -y
+sudo fsck /dev/sda3 -y
+#sudo fsck /dev/sda4 -y
+sudo cryptdisks_stop home-enc | true
+sudo cryptdisks_start home-enc
+sudo fsck -a /dev/mapper/home-enc
+#sudo mkdir /media/tpasch/5e3b3f3a-029c-4d60-9968-acc2c6c1a234
+#sudo mount UUID=5e3b3f3a-029c-4d60-9968-acc2c6c1a234 /media/tpasch/5e3b3f3a-029c-4d60-9968-acc2c6c1a234
+#source /media/tpasch/5e3b3f3a-029c-4d60-9968-acc2c6c1a234/tpasch/.bashrc
+#ln -fs /media/tpasch/5e3b3f3a-029c-4d60-9968-acc2c6c1a234/tpasch/.bashrc ~/
+#sudo swapon UUID="74000971-7dac-48f4-9404-c5759cf478f7" 
+#exec bash
---
+++ home/quabbe/bin/rotate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output DSI-1 --rotate right
