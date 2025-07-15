8011da56b788b32703dff8be9cb163e891b93b64
aanno <aanno@users.noreply.github.com>
2025-07-15 13:25:59 +0200

    update rust dev container

--- dev-in-container/rust/initial-download.sh
+++ dev-in-container/rust/initial-download.sh
@@ -6,3 +6,5 @@ set -xe
 CWD=`pwd`/../../build-with-docker
 
 curl -o rustup.sh --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs
+curl -o install-pdm.py -sSL https://pdm-project.org/install-pdm.py
+curl -o volta.sh https://get.volta.sh
