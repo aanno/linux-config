ea28bda7d7ab52be8429b70c2f3b6940bfd4ceac
aanno <aanno@users.noreply.github.com>
2025-08-20 15:00:50 +0200

    update in rust-python

--- dev-in-container/rust-python/run-once.sh
+++ dev-in-container/rust-python/run-once.sh
@@ -4,18 +4,18 @@
 
 pnpm install -g @anthropic-ai/claude-code
 
-sudo chown -R vscode:vscode /home/vscode/.cargo/
+# sudo chown -R vscode:vscode /home/vscode/.cargo/
 ln -s /home/vscode/.local/share/pnpm/global/5/node_modules/\@anthropic-ai/claude-code/cli.js ~/.volta/bin/claude
 
-pip install --upgrade pip
-pip install pre-commit maturin psycopg "psycopg[pool]" pgvector "sentence-transformers"
+python -m pip install --force-reinstall pip
+pip install pre-commit maturin psycopg "psycopg[pool]" pgvector "sentence-transformers" "huggingface_hub[cli]"
 # ensure that this is _not_ saved in .local
 
-sudo chown -R vscode:vscode /home/vscode/.cargo/
+# sudo chown -R vscode:vscode /home/vscode/.cargo/
 
 # must be done in the git repo
 # ~/.local/bin/pre-commit install
 
-huggingface-cli download sentence-transformers/all-MiniLM-L6-v2
-huggingface-cli download microsoft/graphcodebert-base
-huggingface-cli download microsoft/unixcoder-base
+hf download sentence-transformers/all-MiniLM-L6-v2
+hf download microsoft/graphcodebert-base
+hf download microsoft/unixcoder-base
