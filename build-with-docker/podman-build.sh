1c216d45f07256ce0bd64f638c1701daacc6bfec
aanno <aanno@users.noreply.github.com>
2025-10-28 14:07:19 +0100

    upmpdcli for f43

--- build-with-docker/podman-build.sh
+++ build-with-docker/podman-build.sh
@@ -24,14 +24,19 @@ podman build --pull -t owntone \
   -v $CWD/opt:/opt:z \
   -v $CWD/.cargo:/root/.cargo:z \
   -v $CWD/.rustup:/root/.rustup:z \
-  -f Containerfile.spotifyd.fedora43;
+  -f Containerfile.upmpdcli.fedora43;
 
 # all except ..
 # tar cvfz owntone.tar.gz ./opt/
 
 # ... upmpdcli
 
-# cp opt/owntone/make-it-longer/upmpdcli/upmpdcli-1.9.3.tar.xz .
+cp opt/owntone/make-it-longer/upmpdcli/upmpdcli-1.9.6.tar.xz .
+rm -rf usr upmpdcli.tar.gz
+tar xvfJ upmpdcli-1.9.6.tar.xz
+tar cvfz upmpdcli.tar.gz ./usr
+cp upmpdcli-fedora.fpm .fpm
+sudo /root/.local/share/gem/ruby/3.3.0/bin/fpm
 
 # ... taglib-2
 
@@ -70,11 +75,11 @@ podman build --pull -t owntone \
 # only for spotifyd
 ## TODO: find container name ('relaxed_torvalds' here)
 ## podman cp relaxed_torvalds:/opt/owntone/release/spotifyd .
-mkdir -p usr/bin/
-cp ./opt/owntone/spotifyd usr/bin/
-tar cvfz spotifyd.tar.gz ./usr
-cp -f spotifyd-fedora.fpm .fpm
-sudo /root/.local/share/gem/ruby/3.3.0/bin/fpm
+# mkdir -p usr/bin/
+# cp ./opt/owntone/spotifyd usr/bin/
+# tar cvfz spotifyd.tar.gz ./usr
+# cp -f spotifyd-fedora.fpm .fpm
+# sudo /root/.local/share/gem/ruby/3.3.0/bin/fpm
 
 # galera-4
 # cp opt/owntone/make-it-longer/galera-4-26.4.18/galera-4-26.4.18.tgz .
