b86a5e66296b9ccee404f4f9f1cf1c826c854c1b
aanno <aanno@users.noreply.github.com>
2026-01-17 19:20:14 +0100

    inital devcontainer python (still with errors)

---
+++ dev-in-container/python/run-once.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+source /home/vscode/.bashrc
+. ~/.venv/bin/activate
+
+~/.volta/bin/pnpm set -g store-dir /pnpm
+~/.volta/bin/pnpm setup
+
+source /home/vscode/.bashrc
+pnpm install -g @anthropic-ai/claude-code
+pip install pre-commit maturin psycopg "psycopg[pool]" pgvector \
+  "sentence-transformers" 
+
+pnpm install -g @anthropic-ai/claude-code
+
+# sudo chown -R vscode:vscode /home/vscode/.cargo/
+ln -s /home/vscode/.local/share/pnpm/global/5/node_modules/\@anthropic-ai/claude-code/cli.js ~/.volta/bin/claude
+
+python -m pip install --force-reinstall pip
+# pip install pre-commit maturin psycopg "psycopg[pool]" pgvector "sentence-transformers" "huggingface_hub[cli]"
+# ensure that this is _not_ saved in .local
+
+# sudo chown -R vscode:vscode /home/vscode/.cargo/
+
+# must be done in the git repo
+# ~/.local/bin/pre-commit install
+
+#hf download sentence-transformers/all-MiniLM-L6-v2
+#hf download microsoft/graphcodebert-base
+#hf download microsoft/unixcoder-base
